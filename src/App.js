a050d87e53e2acce958d405f51691a3972cb5abc
Bhupas Gautam <64760375+bhupas@users.noreply.github.com>
2023-04-23 19:59:16 +0000

    hopefully last commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Main from './Components/Main';
-import './Components/style.css'
+import './Components/style.css';
+import AboutUs from './Components/AboutUs';
+
 function App() {
   return (
-    <>
-      <Main/>
-    </>
+    <Router>
+      <div>
+        <nav style={{ backgroundColor: '#333', color: '#fff' }}>
+          <ul style={{ display: 'flex', justifyContent: 'space-between', listStyle: 'none', margin: 0, padding: 0 }}>
+            <li style={{ margin: '0 10px' }}>
+              <Link to="/" style={{ color: '#fff', textDecoration: 'none', fontSize: 18 }}>Home</Link>
+            </li>
+            <li style={{ margin: '0 10px' }}>
+              <Link to="/about" style={{ color: '#fff', textDecoration: 'none', fontSize: 18 }}>About Us</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h2>About Us</h2>
+      <p>We are a team of passionate individuals dedicated to creating high-quality software products that meet the needs of our clients.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
